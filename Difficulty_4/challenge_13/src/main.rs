70ea8a6bfece24a0ff79a77c17a9897c95231372
Max <85294320+MaxTheRock@users.noreply.github.com>
2025-03-28 18:34:57 +0000

    Update main.rs for challenge 13

--- Difficulty_4/challenge_13/src/main.rs
+++ Difficulty_4/challenge_13/src/main.rs
@@ -1,12 +1,23 @@
 use std::io::{self, Write};
 
 fn main() {
-    let _holiday_time: i8 = 28;
-    let choice: String = input("Are you working full time: ");
+    let choice: String = input("Are you working full time or part time: ");
 
-    let full_time: bool = if choice.to_lowercase() == "yes" {true} else {false};
-    let work_days: i8 = int(&input("How many days do you work a week: "));
-    
+    let full_time: bool = if choice.to_lowercase() == "full" {
+        true
+    } else  {
+        false
+    };
+    let work_days: f32 = float(int(&input("How many days do you work a week: ")));
+    let mut holiday_time: f32 = 0.0;
+    if full_time && work_days == 5.0 {
+        holiday_time = 28.0;
+    } else if !full_time {
+        let multiplyer: f32 = work_days / 5.0;
+        holiday_time = 28.0 * multiplyer;
+    };
+
+    println!("Holiday time allowed: {} days",holiday_time);
     
 }
 
@@ -20,4 +31,8 @@ fn input(print_text: &str) -> String {
 
 fn int(string: &str) -> i8 {
     string.parse().expect("Please enter a valid number")
+}
+
+fn float(int: i8) -> f32 {
+    int as f32
 }
